1e726c962a97abf91e28cb76cea16721c00233ba
Randy Klein <randyklein99@gmail.com>
2023-03-06 09:30:02 -0500

    played with static v non-static methods

--- examples.java
+++ examples.java
@@ -7,7 +7,26 @@ public class examples {
         System.out.println(roundValue(0.23));
     }
 
+    // A static method belongs to the class as well as the instance, but accessing
+    // the method from the instance works but is discouraged.
     public static double roundValue(double value) {
+
+        double realValue;
+
+        double rem = value % .25;
+
+        if (rem != 0.0) {
+            realValue = value - rem + .25;
+        } else {
+            realValue = value;
+        }
+
+        return realValue;
+    }
+
+    // A non-static method belongs to only the instance and can only be accessed by
+    // creating an instance
+    public double roundValue2(double value) {
 
         double realValue;
 
--- testExamples.java
+++ testExamples.java
@@ -12,10 +12,21 @@ public class testExamples {
         new examples();
     }
 
+    // roundValue is a static method and be called without creating an instance
     @Test
     public void testRoundValue() {
         // fail("Not yet implemented");
         assertEquals(0.25, examples.roundValue(0.2));
+    }
+
+    // roundValue2 is not a static method and therefore an instance must be created
+    // first
+    @Test
+    public void testRoundValue2() {
+        // fail("Not yet implemented");
+        examples myObj = new examples();
+        assertEquals(0.25, myObj.roundValue2(0.2));
+        assertEquals(0.25, myObj.roundValue2(0.25));
     }
 
 }

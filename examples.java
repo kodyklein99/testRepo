37728184dc8ca34998d19896703acb66a9a1197c
Randy Klein <randyklein99@gmail.com>
2023-03-06 10:09:24 -0500

    created a private method for round value

--- examples.java
+++ examples.java
@@ -1,6 +1,9 @@
 
 public class examples {
 
+    // The main method is what runs when you call the class. Similar to python if
+    // name == main. Without a main method, you can't run it and can only create
+    // instances.
     public static void main(String[] args) {
         // examples myObj = new examples();
 
@@ -9,25 +12,17 @@ public class examples {
 
     // A static method belongs to the class as well as the instance, but accessing
     // the method from the instance works but is discouraged.
-    public static double roundValue(double value) {
-
-        double realValue;
-
-        double rem = value % .25;
-
-        if (rem != 0.0) {
-            realValue = value - rem + .25;
-        } else {
-            realValue = value;
-        }
-
-        return realValue;
+    public static double staticRoundValue(double value) {
+        return roundValue(value);
     }
 
     // A non-static method belongs to only the instance and can only be accessed by
     // creating an instance
-    public double roundValue2(double value) {
+    public double notStaticRoundValue(double value) {
+        return roundValue(value);
+    }
 
+    private static double roundValue(double value) {
         double realValue;
 
         double rem = value % .25;
--- testExamples.java
+++ testExamples.java
@@ -14,19 +14,32 @@ public class testExamples {
 
     // roundValue is a static method and be called without creating an instance
     @Test
-    public void testRoundValue() {
+    public void testStaticRoundValue() {
         // fail("Not yet implemented");
-        assertEquals(0.25, examples.roundValue(0.2));
+        assertEquals(0.25, examples.staticRoundValue(0.2));
+
     }
 
     // roundValue2 is not a static method and therefore an instance must be created
     // first
     @Test
-    public void testRoundValue2() {
+    public void testnotStaticRoundValue() {
         // fail("Not yet implemented");
         examples myObj = new examples();
-        assertEquals(0.25, myObj.roundValue2(0.2));
-        assertEquals(0.25, myObj.roundValue2(0.25));
+        assertEquals(0.25, myObj.notStaticRoundValue(0.2));
+        assertEquals(0.25, myObj.notStaticRoundValue(0.25));
+
     }
+
+    /**
+     * This test fails because it is trying to access a private method from outside
+     * the class.
+     * 
+     * @Test
+     *       public void testRoundValue() {
+     *       assertEquals(0.25, examples.roundValue(0.2));
+     *       assertEquals(0.25, myObj.roundValue(0.25));
+     *       }
+     **/
 
 }

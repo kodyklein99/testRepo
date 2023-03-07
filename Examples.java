f7d1903ef127f0a8c86dec8827b4463e9f307921
Randy Klein <randyklein99@gmail.com>
2023-03-07 09:46:27 -0500

    added constructor and cleaned up

--- Examples.java
+++ Examples.java
@@ -1,24 +1,33 @@
 
 public class Examples {
+    double value;
+
+    public Examples(double value) {
+        this.value = value;
+
+    }
 
     // The main method is what runs when you call the class. Similar to python if
     // name == main. Without a main method, you can't run it and can only create
     // instances.
     public static void main(String[] args) {
         // examples myObj = new examples();
+        Examples myObj = new Examples(0.23);
 
-        System.out.println(roundValue(0.23));
+        System.out.println(myObj.notStaticRoundValue());
     }
 
-    // A static method belongs to the class as well as the instance, but accessing
-    // the method from the instance works but is discouraged.
-    public static double staticRoundValue(double value) {
-        return roundValue(value);
-    }
+    /*
+     * A static method belongs to the class as well as the instance, but accessing
+     * // the method from the instance works but is discouraged.
+     * public static double staticRoundValue(double value) {
+     * return roundValue(value);
+     * }
+     */
 
     // A non-static method belongs to only the instance and can only be accessed by
     // creating an instance
-    public double notStaticRoundValue(double value) {
+    public double notStaticRoundValue() {
         return roundValue(value);
     }
 
--- TestExamples.java
+++ TestExamples.java
@@ -9,25 +9,29 @@ public class TestExamples {
      */
     @Test
     public void testDefaultConstructor() {
-        new Examples();
+        new Examples(0.23);
     }
 
     // roundValue is a static method and be called without creating an instance
-    @Test
-    public void testStaticRoundValue() {
-        // fail("Not yet implemented");
-        assertEquals(0.25, Examples.staticRoundValue(0.2));
-
-    }
+    /*
+     * @Test
+     * public void testStaticRoundValue() {
+     * // fail("Not yet implemented");
+     * assertEquals(0.25, Examples.staticRoundValue(0.2));
+     * 
+     * }
+     */
 
     // roundValue2 is not a static method and therefore an instance must be created
     // first
     @Test
-    public void testnotStaticRoundValue() {
+    public void testNotStaticRoundValue() {
         // fail("Not yet implemented");
-        Examples myObj = new Examples();
-        assertEquals(0.25, myObj.notStaticRoundValue(0.2));
-        assertEquals(0.25, myObj.notStaticRoundValue(0.25));
+        Examples myObj = new Examples(0.23);
+        assertEquals(0.25, myObj.notStaticRoundValue());
+
+        Examples myObj2 = new Examples(0.25);
+        assertEquals(0.25, myObj2.notStaticRoundValue());
 
     }
 
